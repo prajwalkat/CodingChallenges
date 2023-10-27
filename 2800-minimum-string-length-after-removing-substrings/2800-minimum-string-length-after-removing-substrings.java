e4321c8b374ecbef4f7d260315d2d497f190f74a
D4_80810_PrajwalKatkar <69921356+prajwalkat@users.noreply.github.com>
2023-10-27 14:57:26 +0530

    Time: 7 ms (42.13%), Space: 43.7 MB (33.15%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minLength(String s) {
+        while(s.contains("AB") || s.contains("CD")){
+            s=s.replace("AB","");
+        s=s.replace("CD","");
+ 
+        }
+        
+        s=s.replace("AB","");
+        s=s.replace("CD","");
+
+
+        return s.length();
+    }
+}
