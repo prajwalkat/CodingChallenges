52879553998421080da68c52ccc5314bab5d0512
D4_80810_PrajwalKatkar <69921356+prajwalkat@users.noreply.github.com>
2023-10-30 15:25:14 +0530

    Time: 0 ms (100%), Space: 38.7 MB (95.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,5 @@
 class Solution {
+    int[] f=new int[46];
     public int climbStairs(int n) {
         // if(n==1){
         //     return 1;
@@ -9,12 +10,35 @@ class Solution {
 
         // return climbStairs(n-1)+climbStairs(n-2);
 
-        double phi = ((1-Math.sqrt(5))/2);
-		double Phi = ((1+Math.sqrt(5))/2);
-		double s5=Math.sqrt(5);
+        // double phi = ((1-Math.sqrt(5))/2);
+		// double Phi = ((1+Math.sqrt(5))/2);
+		// double s5=Math.sqrt(5);
 		
-		int result=(int)((Math.pow(Phi,n+1)-Math.pow(phi,n+1))/s5);
-		return result;
+		// int result=(int)((Math.pow(Phi,n+1)-Math.pow(phi,n+1))/s5);
+		// return result;
+
+	if (n ==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    if(n==2){
+        return 2;
+    }
+
+	
+	
+	if (f[n] != 0)
+		return f[n];
+	
+	// calculating fib_term(n)
+	else {
+		f[n] = climbStairs(n - 1) + climbStairs(n - 2);
+		return f[n];
+	}
+
+
 
 
     }
