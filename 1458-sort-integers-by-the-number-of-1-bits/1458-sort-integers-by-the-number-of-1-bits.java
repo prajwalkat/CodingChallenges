9a2926392f60cac25ee23fe916ed2c6e1c4dae6b
D4_80810_PrajwalKatkar <69921356+prajwalkat@users.noreply.github.com>
2023-10-30 18:15:13 +0530

    Time: 10 ms (42.5%), Space: 43.4 MB (95.11%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.java
@@ -1,27 +1,64 @@
+class cc implements Comparator<Integer>{
+     @Override
+    public int compare(Integer o1, Integer o2) {
+       if(Integer.bitCount(o1)<Integer.bitCount(o2)){
+
+           return -1;
+       }
+       
+       if(Integer.bitCount(o1)>Integer.bitCount(o2)){
+           return 1;
+       }
+       if(Integer.bitCount(o1)==Integer.bitCount(o2)){
+           if(o1<o2){return -1;}
+           if(o1>o2){return 1;}
+           if(o1==o2){return 0;}
+       }
+       
+       return 0;
+       
+    }
+}
+
+
 class Solution {
     public int[] sortByBits(int[] a) {
-        Arrays.sort(a);
-        int[] cb=new int[a.length];
-        ArrayList<Integer> b=new ArrayList<Integer>();
-                for(int i=0;i<a.length;i++){
-                    cb[i]=Integer.bitCount(a[i]);
-                }
+        // Arrays.sort(a);
+        // int[] cb=new int[a.length];
+        // ArrayList<Integer> b=new ArrayList<Integer>();
+        //         for(int i=0;i<a.length;i++){
+        //             cb[i]=Integer.bitCount(a[i]);
+        //         }
 
-         for(int i=0;i<18;i++){
-             for(int j=0;j<a.length;j++){
-                 if(cb[j]==i){
-                     b.add(a[j]);
-                 }
-             }
+        //  for(int i=0;i<18;i++){
+        //      for(int j=0;j<a.length;j++){
+        //          if(cb[j]==i){
+        //              b.add(a[j]);
+        //          }
+        //      }
 
-         }   
-         int f[]=new int[a.length];
-         Object x[]=b.toArray();
-        //  System.out.println(Arrays.toString(x));
-        for(int i=0;i<a.length;i++){
-            f[i]=((Integer)x[i]).intValue();
+        //  }   
+        //  int f[]=new int[a.length];
+        //  Object x[]=b.toArray();
+        // //  System.out.println(Arrays.toString(x));
+        // for(int i=0;i<a.length;i++){
+        //     f[i]=((Integer)x[i]).intValue();
+        // }
+        //  b.clear();
+        //  return f;
+
+        cc is=new cc();
+
+        
+        Integer[] b = Arrays.stream(a).boxed().toArray( Integer[]::new );
+        Arrays.sort(b,is);
+       // System.out.println(Arrays.toString(b));
+
+        for(int i=0;i<b.length;i++){
+            a[i]=b[i];
         }
-         b.clear();
-         return f;
+        return a;
+
+
     }
 }
