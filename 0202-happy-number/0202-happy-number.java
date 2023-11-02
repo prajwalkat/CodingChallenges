2d5f0855fa10637dd8b2bb3ad463b974c2f620e5
D4_80810_PrajwalKatkar <69921356+prajwalkat@users.noreply.github.com>
2023-11-02 16:08:52 +0530

    Time: 1 ms (82.91%), Space: 39.7 MB (33.07%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isHappy(int n) {
-        int[] sq=new int[]{0,1,4,9,16,25,36,49,64,81};
+        
         int x=0;
         HashSet<Integer> s=new HashSet<>();
          while(x!=n){
@@ -9,7 +9,7 @@ class Solution {
             
             while(z!=0){
                 int y=z%10;
-                sum=sum+sq[y];
+                sum=sum+y*y;
                 z=z/10;
                 
             }
