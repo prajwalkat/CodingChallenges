249f890d5911f849eb987684a21287c77720961a
D4_80810_PrajwalKatkar <69921356+prajwalkat@users.noreply.github.com>
2023-11-24 07:04:50 +0530

    Time: 5 ms (99.12%), Space: 56.8 MB (6.17%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -1,15 +1,57 @@
 class Solution {
     public int maxCoins(int[] p) {
-        Arrays.sort(p);
+        countSort(p,p.length);
         int n=p.length/3;
         int sum=0;
         for(int i=n;i<p.length;i+=2){
-            System.out.println(p[i]);
+           
             sum=sum+p[i];
         }
         
         return sum;
     }
+    
+    void countSort(int array[], int size) {
+    int[] output = new int[size + 1];
+
+    // Find the largest element of the array
+    int max = array[0];
+    for (int i = 1; i < size; i++) {
+      if (array[i] > max)
+        max = array[i];
+    }
+    int[] count = new int[max + 1];
+
+    // Initialize count array with all zeros.
+    for (int i = 0; i < max; ++i) {
+      count[i] = 0;
+    }
+
+    // Store the count of each element
+    for (int i = 0; i < size; i++) {
+      count[array[i]]++;
+    }
+
+    // Store the cummulative count of each array
+    for (int i = 1; i <= max; i++) {
+      count[i] += count[i - 1];
+    }
+
+    // Find the index of each element of the original array in count array, and
+    // place the elements in output array
+    for (int i = size - 1; i >= 0; i--) {
+      output[count[array[i]] - 1] = array[i];
+      count[array[i]]--;
+    }
+
+    // Copy the sorted elements into original array
+    for (int i = 0; i < size; i++) {
+      array[i] = output[i];
+    }
+  }
+    
+    
+    
 }
 
 
